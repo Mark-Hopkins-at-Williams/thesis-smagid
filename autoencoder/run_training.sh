c41ced84a5f17bf58902457b8e148755c3c95b52
Sam Magid <samptmagid@gmail.com>
2024-10-08 11:46:33 -0400

    some work!

--- autoencoder/run_training.sh
+++ autoencoder/run_training.sh
@@ -3,8 +3,8 @@
 #SBATCH -t 0-20:00
 #SBATCH -p dl
 #SBATCH --mem=10G
-#SBATCH -o myoutput_%j.out
-#SBATCH -e myerrors_%j.err
+#SBATCH -o logs/log%j.out
+#SBATCH -e logs/log%j.err
 #SBATCH --gres=gpu:2
 
 python training.py
--- autoencoder/training.py
+++ autoencoder/training.py
@@ -6,22 +6,17 @@ import pandas as pd
 import torchvision
 import torchvision.transforms as transforms
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset, DataLoader, random_split
 from tqdm import tqdm
+import sys
 
 IMG_DIR = 'fontdata/images'
 CSV_DATA = 'fontdata/data.csv'
-IMG_SIZE = 28
-DATA_RANGE = 500
+IMG_SIZE = 128
+DATA_RANGE = None
 BATCH_SIZE = 256
 NUM_EPOCHS = 100
-  
-# Initializing the transform for the dataset 
-transform = torchvision.transforms.Compose([ 
-    torchvision.transforms.ToTensor(), 
-    torchvision.transforms.Normalize((0.5), (0.5)) 
-]) 
-  
+
 class FontDataset(Dataset):
     def __init__(self, csv_file, img_dir, transform = None, range = None):
         self.data_frame = pd.read_csv(csv_file)
@@ -41,41 +36,39 @@ class FontDataset(Dataset):
         label = self.data_frame.iloc[idx, 1]
         
         if self.transform:
-            image = self.transform(image)\
+            image = self.transform(image)
 
         return image, label
 
-# defining transforms
-transforms = transforms.Compose([
+transformations = transforms.Compose([
     transforms.Resize((IMG_SIZE, IMG_SIZE)),
+    torchvision.transforms.Grayscale(num_output_channels=1),
     transforms.ToTensor(), 
-    transforms.Normalize((0.5), (0.5)) 
+    transforms.Normalize((0.5), (0.5)),
 ])
   
-# create and load dataset
-font_dataset = FontDataset(csv_file = CSV_DATA, img_dir = IMG_DIR,
-                            transform = transforms, range = DATA_RANGE)
-train_loader = torch.utils.data.DataLoader( 
-    font_dataset, batch_size=BATCH_SIZE) 
-  
-# Printing 25 random images from the training dataset 
-# random_samples = np.random.randint( 
-#     1, len(font_dataset), (25))
-
-# for idx in range(random_samples.shape[0]): 
-#     plt.subplot(5, 5, idx + 1) 
-#     plt.imshow(font_dataset[idx][0][0].numpy(), cmap='gray') 
-#     plt.title(font_dataset[idx][1]) 
-#     plt.axis('off') 
-
-# plt.tight_layout() 
-# plt.savefig('test_data.png')
+# Loading data, reserving 10% for a devset
+full_dataset = FontDataset(csv_file = CSV_DATA, img_dir = IMG_DIR,
+                            transform = transformations, range = DATA_RANGE)
+train_size = int(0.9 * len(full_dataset))
+dev_size = len(full_dataset) - train_size
+train_dataset, dev_dataset = random_split(full_dataset, [train_size, dev_size])
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=BATCH_SIZE) 
+dev_loader = torch.utils.data.DataLoader(dev_dataset, batch_size=BATCH_SIZE) 
 
 class DeepAutoencoder(torch.nn.Module): 
     def __init__(self): 
         super().__init__()         
         self.encoder = torch.nn.Sequential( 
-            torch.nn.Linear(IMG_SIZE * IMG_SIZE, 256), 
+            torch.nn.Linear(IMG_SIZE*IMG_SIZE, 6272), 
+            torch.nn.ReLU(),
+            torch.nn.Linear(6272, 3136), 
+            torch.nn.ReLU(),
+            torch.nn.Linear(3136, 1568), 
+            torch.nn.ReLU(),
+            torch.nn.Linear(1568, 784), 
+            torch.nn.ReLU(), 
+            torch.nn.Linear(784, 256), 
             torch.nn.ReLU(), 
             torch.nn.Linear(256, 128), 
             torch.nn.ReLU(), 
@@ -91,7 +84,15 @@ class DeepAutoencoder(torch.nn.Module):
             torch.nn.ReLU(), 
             torch.nn.Linear(128, 256), 
             torch.nn.ReLU(), 
-            torch.nn.Linear(256, IMG_SIZE * IMG_SIZE), 
+            torch.nn.Linear(256, 784),
+            torch.nn.ReLU(), 
+            torch.nn.Linear(784, 1568),
+            torch.nn.ReLU(), 
+            torch.nn.Linear(1568, 3136),
+            torch.nn.ReLU(), 
+            torch.nn.Linear(3136, 6272),
+            torch.nn.ReLU(), 
+            torch.nn.Linear(6272, IMG_SIZE*IMG_SIZE), 
             torch.nn.Sigmoid() 
         ) 
   
@@ -100,69 +101,88 @@ class DeepAutoencoder(torch.nn.Module):
         decoded = self.decoder(encoded) 
         return decoded 
 
-# Instantiating the model and hyperparameters 
+# save an image of model progress on a sample of devset
+def image_progress(dev_set, model, step):
+    sample = 5
+    img, _ = dev_set
+    img = img.reshape(-1, IMG_SIZE*IMG_SIZE)
+    img = img.to("cuda")
+    original = img.reshape(-1,IMG_SIZE,IMG_SIZE)[0]
+    model.eval()
+    with torch.no_grad():
+        out = model(img)
+        out = out.to("cuda")
+    trained = out.reshape(-1,IMG_SIZE,IMG_SIZE)[0:sample]
+    original = img.reshape(-1,IMG_SIZE,IMG_SIZE)[0:sample]
+
+    plt.clf()
+    for i in range(sample):
+        plt.subplot(sample, 2, 2*i + 1)
+        plt.imshow(original[i].cpu().detach().numpy(), cmap='gray')
+        plt.title("original")
+        plt.axis('off')
+        plt.subplot(sample, 2, 2*i + 2)
+        plt.imshow(trained[i].cpu().detach().numpy(), cmap='gray')
+        plt.title("trained")
+        plt.axis('off')
+    plt.tight_layout()
+    plt.savefig(f'progress-112/{step}.png')
+
 model = DeepAutoencoder() 
 criterion = torch.nn.MSELoss() 
 num_epochs = NUM_EPOCHS
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-# List that will store the training loss 
 train_loss = [] 
 
-# Dictionary that will store the 
-# different images and outputs for  
-# various epochs 
-outputs = {}
+num_batches = len(train_loader)
 
-batch_size = len(train_loader)
-
-# Transfer to GPU
+# Transfering model to GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 model.to(device)
 
-# Training loop starts 
-for epoch in tqdm(range(num_epochs)): 
-        
-    # Initializing variable for storing loss
-    running_loss = 0
-      
-    # Iterating over the training dataset 
-    for batch in train_loader: 
-        
-        # Loading image(s) and 
-        # reshaping it into a 1-d vector 
-        img, _ = batch # are we using the labels?
-        img = img.reshape(-1, IMG_SIZE*IMG_SIZE)
+sample_data = next(iter(dev_loader))
+record_every = 10
 
-        # put data on cuda
+steps = 0 # Running count of steps
+
+for epoch in range(num_epochs): 
+    running_loss = 0 # Running loss count
+    for i, batch in tqdm(enumerate(train_loader)): 
+        # Unpacking batch
+        img, _ = batch
+        img = img.reshape(-1, IMG_SIZE*IMG_SIZE)
         img = img.to(device)
-          
-        # Generating output 
+
+        # Running batch through model and update weights
         out = model(img) 
-          
-        # Calculating loss 
-        loss = criterion(out, img) 
-          
-        # Updating weights according 
-        # to the calculated loss 
+        loss = criterion(out, img)
         optimizer.zero_grad() 
         loss.backward() 
         optimizer.step() 
-          
-        # Incrementing loss 
+
+        # increment loss
         running_loss += loss.item() 
-      
+
+        # Recording performance metrics every record_every updates
+        if i % record_every == 0:
+            image_progress(sample_data, model, steps // record_every)
+            if i > 0:
+                loss_per_batch = running_loss / i if i > 0 else 0
+                print(f'Train loss: {loss_per_batch}')
+                sys.stdout.flush()
+
+        # Keeping track of steps
+        steps += 1
+
+              
     # Averaging out loss over entire batch 
-    running_loss /= batch_size 
+    running_loss /= num_batches
     train_loss.append(running_loss) 
-      
-    # Storing useful images and 
-    # reconstructed outputs for the last batch 
-    outputs[epoch+1] = {'img': img, 'out': out} 
 
 # Plotting the training loss 
 plt.plot(range(1,num_epochs+1),train_loss) 
 plt.xlabel("Number of epochs") 
 plt.ylabel("Training Loss") 
-plt.savefig(loss.png)
+plt.savefig('loss.png')
