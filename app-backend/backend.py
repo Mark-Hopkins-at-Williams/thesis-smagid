4a1cda670717bf0c038cd236f333db7afa2d8ec9
sammagid <samptmagid@gmail.com>
2025-01-30 11:32:38 -0500

    it's working! (backend)'

--- app-backend/backend.py
+++ app-backend/backend.py
@@ -1,41 +1,73 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
+import numpy as np
 import torch
+import faiss
+
+Z_SIZE = 6
+DATA_FILE = 'merged.csv'
 
 app = Flask(__name__)
+CORS(app)
 
-data = pd.read_csv('srivatsan-6.csv')
+# set up faiss
+data = pd.read_csv(DATA_FILE)
+fontNames = data["font"].values
+# remove attribute data
+labels = ["font", "z0","z1","z2","z3","z4","z5"]
+data = data[labels]
+zLabels = ["z0","z1","z2","z3","z4","z5"]
+vectors = data[zLabels].values.astype("float32")
+faissSearch = faiss.IndexFlatL2(Z_SIZE)
+faissSearch.add(vectors)
 
 @app.route('/getfont', methods=['GET'])
 def get_data():
     centerFont = request.args.get('center')  # Retrieve 'param1' from the URL
-    magnitude = int(request.args.get('mag'))  # Retrieve 'param2' from the URL
-
-    if centerFont in data['font'].values:
-        datumRow = data[data['font'] == centerFont].iloc[:, 1:]
-        datum = torch.tensor(datumRow.values)
-        selectedFonts = []
-        for i in range(datum.shape[1]):
-            modTensor = torch.zeros_like(datum)
-            print(modTensor.shape)
-            modTensor[0, i] = 1 * magnitude
-            newTensor = datum + modTensor
-            minDistance = 9999
-            minFont = None
-            for otherFont in data['font'].values:
-                otherDatum = torch.tensor(data[data['font'] == otherFont].iloc[:,1:].values)
-                distance = torch.sqrt(torch.sum((newTensor - otherDatum) ** 2))
-                if distance < minDistance and otherFont != centerFont and otherFont not in selectedFonts and str(otherFont) != 'nan':
-                    print('help!')
-                    minDistance = distance
-                    minFont = otherFont
-                    print(minFont)
-            selectedFonts.append(str(minFont))
-            print(selectedFonts)
-        return f'Found these fonts: [{', '.join(selectedFonts)}]'
-
+    magnitude = float(request.args.get('mag'))  # Retrieve 'param2' from the URL
+    if centerFont is not None:
+        if centerFont in data['font'].values:
+            datumRow = data[data['font'] == centerFont].iloc[:, 1:]
+            datum = torch.tensor(datumRow.values)
+        else:
+            return
     else:
-        return 'Font not found.'
+        # select random center and magnitude 1
+        datumRow = data.sample()
+        centerFont = datumRow['font'].values[0]
+        print(centerFont)
+        datum = torch.tensor(datumRow.iloc[:, 1:].values)
+    response = dict()
+    response["centerFont"] = centerFont
+    selectedFonts = []
+    for i in range(datum.shape[1]):
+        modTensor = torch.zeros_like(datum)
+        modTensor[0, i] = 1 * magnitude
+        newTensor = datum + modTensor
+        queryVector = newTensor.numpy().astype("float32")
+        print(queryVector)
+        _, indices = faissSearch.search(queryVector, 10)
+        selectedFont = None
+        i = 0
+        # try to avoid duplicates and same fonts
+        # print(indices)
+        while not selectedFont:
+            index = indices[0][i]
+            font = fontNames[index]
+            # print(font)
+            if font != centerFont and font not in selectedFonts:
+                selectedFont = font
+            if i == 9 and selectedFont is None: # last resort just choose the first one
+                print('resorting :(')
+                selectedFont = fontNames[indices[0][0]]
+            i += 1
+        selectedFonts.append(selectedFont)
+    response["selectedFonts"] = selectedFonts
+    return jsonify(response)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=18812)
