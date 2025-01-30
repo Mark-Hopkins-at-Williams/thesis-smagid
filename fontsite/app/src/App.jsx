1505739bab7473ccd0df2824de842e0811f4711e
Sam Magid <sam@samBook.local>
2025-01-30 11:31:19 -0500

    it's working! (frontend)

--- fontsite/app/src/App.jsx
+++ fontsite/app/src/App.jsx
@@ -1,10 +1,67 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 import googlefonts from "./assets/googlefonts.json"
 import GoogleFontLoader from 'react-google-font'
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 import Slider from '@mui/material/Slider';
+import { styled } from '@mui/material/styles';
+
+const iOSBoxShadow =
+  '0 3px 1px rgba(0,0,0,0.1),0 4px 8px rgba(0,0,0,0.13),0 0 0 1px rgba(0,0,0,0.02)';
+
+const IOSSlider = styled(Slider)(({ theme }) => ({
+  color: '#828282',
+  height: 5,
+  padding: '15px 0',
+  '& .MuiSlider-thumb': {
+    height: 20,
+    width: 20,
+    backgroundColor: '#fff',
+    boxShadow: '0 0 2px 0px rgba(0, 0, 0, 0.1)',
+    '&:focus, &:hover, &.Mui-active': {
+      boxShadow: '0px 0px 3px 1px rgba(0, 0, 0, 0.1)',
+      // Reset on touch devices, it doesn't add specificity
+      '@media (hover: none)': {
+        boxShadow: iOSBoxShadow,
+      },
+    },
+    '&:before': {
+      boxShadow:
+        '0px 0px 1px 0px rgba(0,0,0,0.2), 0px 0px 0px 0px rgba(0,0,0,0.14), 0px 0px 1px 0px rgba(0,0,0,0.12)',
+    },
+  },
+  '& .MuiSlider-valueLabel': {
+    fontSize: 16,
+    fontWeight: 'normal',
+    top: -6,
+    backgroundColor: 'unset',
+    color: theme.palette.text.primary,
+    '&::before': {
+      display: 'none',
+    },
+    '& *': {
+      background: 'transparent',
+      color: '#000',
+      ...theme.applyStyles('dark', {
+        color: '#fff',
+      }),
+    },
+  },
+  '& .MuiSlider-track': {
+    border: 'none',
+    height: 5,
+  },
+  '& .MuiSlider-rail': {
+    opacity: 0.5,
+    boxShadow: 'inset 0px 0px 4px -2px #000',
+    backgroundColor: '#d0d0d0',
+  },
+  ...theme.applyStyles('dark', {
+    color: '#0a84ff',
+  }),
+}));
+
 
 const GlyphButton = ({ rotation, onClick, label, font_name }) => {
   let tooltipPlacement = 'left'
@@ -65,8 +122,6 @@ const ShuffleButton = ( {onClick} ) => {
 }
 
 const CenterGlyph = ({ label, font_name }) => {
-  console.log(font_name)
-
   return (
     <>
       <GoogleFontLoader fonts={[{font: font_name}]}/>
@@ -78,32 +133,52 @@ const CenterGlyph = ({ label, font_name }) => {
 }
 
 const getRandomFont = () => {
-  const randomIndex = Math.floor(Math.random() * googlefonts.items.length);
-  const randomFont = googlefonts.items[randomIndex].family;
+  const randomIndex = Math.floor(Math.random() * googlefonts.items.length)
+  const randomFont = googlefonts.items[randomIndex].family
   return randomFont
-};
+}
 
 const historyStack = []
 
 const App = () => {
-  // font state for all buttons
-  const [fonts, setFonts] = useState(Array(6).fill().map(() => getRandomFont()));
-  const [centerFont, setCenterFont] = useState(getRandomFont())
-  const [sliderValue, setSliderValue] = useState(0)
+  const [magnitude, setMagnitude] = useState(0)
+  const [data, setData] = useState(null)
+  const [fonts, setFonts] = useState(Array(6))
+  const [centerFont, setCenterFont] = useState("")
+
+  const fetchData = async (mag, font) => {
+    let queryURL = `http://appa.cs.williams.edu:18812/getfont?mag=${mag}`
+    console.log(fonts)
+    if (font !== "") {
+      queryURL += `&center=${encodeURIComponent(font)}`
+    }
+    console.log(queryURL)
+    fetch(queryURL)
+      .then((response) => response.json())
+      .then((data) => {
+        setCenterFont(data.centerFont)
+        setFonts(data.selectedFonts)
+      })
+      .catch((error) => console.error("Error fetching data:", error))
+  }
+
+  useEffect(() => {
+    fetchData(magnitude, centerFont)
+  }, [historyStack])
 
   const shuffle = () => {
-    historyStack.push([...fonts, centerFont]) // save fonts to history
-    const newCenterFont = getRandomFont()
-    setCenterFont(newCenterFont)
-    const newFonts = Array(6).fill().map(() => getRandomFont())
-    setFonts(newFonts)
+    historyStack.push([...fonts, centerFont, magnitude]) // save to history
+    setMagnitude(0)
+    fetchData(magnitude, "")
   }
 
   const back = () => {
     if (historyStack.length > 0) {
       const lastFonts = historyStack.pop()
+      const magnitude = lastFonts.pop()
       const lastCenterFont = lastFonts.pop()
-      console.log('backing up' + lastCenterFont)
+      console.log('backing up')
+      setMagnitude(magnitude)
       setCenterFont(lastCenterFont)
       setFonts(lastFonts)
     } else {
@@ -112,16 +187,15 @@ const App = () => {
   }
 
   const handleClick = (buttonIndex) => {
-    historyStack.push([...fonts, centerFont]) // save fonts to history
+    historyStack.push([...fonts, centerFont, magnitude]) // save to history
     const newCenterFont = fonts[buttonIndex]
-    setCenterFont(newCenterFont)
-    const newFonts = Array(6).fill().map(() => getRandomFont())
-    setFonts(newFonts)
+    fetchData(magnitude, newCenterFont)
   }
 
   const handleSlider = (event, newValue) => {
-    setSliderValue(newValue);
-  };
+    setMagnitude(newValue)
+    fetchData(magnitude, centerFont)
+  }
 
   let letter = "A"
   
@@ -162,20 +236,16 @@ const App = () => {
         </div>
 
         <div className="right-box">
-          <Tooltip
-          title={<Typography sx={{ fontSize: '1rem' }}>Magnitude</Typography>}
-          placement='bottom'
-          >
-            <Slider 
-              className="slider"
-              aria-label="Slider"
-              value={sliderValue}
-              onChange={handleSlider}
-              min={-100}
-              max={100}
-              color="#828282"
-            />
-          </Tooltip>
+          <IOSSlider 
+            className="slider"
+            aria-label="Slider"
+            value={magnitude}
+            onChange={handleSlider}
+            min={-8}
+            max={8}
+            step={0.1}
+            valueLabelDisplay="on"
+          />
         </div>
 
       </div>
